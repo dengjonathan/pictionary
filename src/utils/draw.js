c7b54ff71404da2ab7009cf52e9a6762ecd2c5f2
jonathan.deng <dengjonathan@gmail.com>
2017-02-10 17:20:29 -0800

    refactor (draw): use all reactive operators

--- src/utils/draw.js
+++ src/utils/draw.js
@@ -3,35 +3,32 @@ const Rx = require('rxjs');
 export default function draw() {
   const $canvas = document.getElementById('canvas');
   const ctx = $canvas.getContext('2d');
-
   const mouseDowns = Rx.Observable.fromEvent($canvas, 'mousedown');
   const mouseMoves = Rx.Observable.fromEvent($canvas, 'mousemove');
   const mouseUps = Rx.Observable.fromEvent($canvas, 'mouseup');
 
-  // compose
+  const drawLine = (prevX, prevY, nextX, nextY) => {
+    console.log(prevX, prevY, nextX, nextY);
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(nextX, nextY);
+    ctx.stroke();
+  }
+
   const mousePaths = mouseDowns
     .first()
     .concat(mouseMoves.takeUntil(mouseUps))
+    .scan((prev, ev) => ({
+      prev: prev.current,
+      current: [ev.x - $canvas.offsetLeft, ev.y - $canvas.offsetTop]
+    }), {})
+    .skip(1)
     .repeat();
 
-  let prevX, prevY;
-  mousePaths.forEach(e => {
-    const x = e.x - $canvas.offsetLeft;
-    const y = e.y - $canvas.offsetTop;
-    if (prevX) {
-      ctx.beginPath();
-      ctx.moveTo(prevX, prevY);
-      ctx.lineTo(x, y);
-      ctx.stroke();
-      ctx.closePath();
-    }
-    prevX = x;
-    prevY = y;
+  mousePaths.forEach(diff => {
+    console.log(diff);
+    drawLine(...diff.prev, ...diff.current);
   });
 
-  mouseUps.forEach(e => {
-    prevX = null;
-    prevY = null;
-  });
-
+  drawLine(0, 800, 100, 800)
 }
