c2d97230503ab9b34f52d0c836de62d031d8413d
jonathan.deng <dengjonathan@gmail.com>
2017-02-14 13:00:53 -0800

    feat (Navigation): add bottom nav bar

--- src/components/AppRoot/App.js
+++ src/components/AppRoot/App.js
@@ -3,6 +3,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import HeaderBar from '../AppBar/AppBar';
 import Canvas from '../Canvas/Canvas';
+import Navigation from '../Navigation/Navigation';
 import './App.css';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <div className="App">
           <HeaderBar/>
           <Canvas/>
+          <Navigation />
         </div>
       </MuiThemeProvider>
     );
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import BottomNavigation from 'material-ui/BottomNavigation';
+import BottomNavigationItem from 'material-ui/BottomNavigation';
+
+const Navigation = () => (
+  <BottomNavigation>
+    <BottomNavigation label='hello' />
+  </BottomNavigation>
+);
+
+export default Navigation;
