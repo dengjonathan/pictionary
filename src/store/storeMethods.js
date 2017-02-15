14dc6a291c4ca28c4af646e75bc3cb68a392c0a7
jonathan.deng <dengjonathan@gmail.com>
2017-02-15 11:51:23 -0800

    feat (store): add abstract methods to update store

---
+++ src/store/storeMethods.js
@@ -0,0 +1,11 @@
+import store from './store';
+
+export const getState = key => {
+  const state = store.getState();
+  if (key in state) {
+    return state.key;
+  }
+  return state;
+};
+
+export const dispatch = action => store.dispatch(action);
