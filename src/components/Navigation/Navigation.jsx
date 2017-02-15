742924650289c2317779f3bc6f5033b56955f2e8
jonathan.deng <dengjonathan@gmail.com>
2017-02-14 19:07:27 -0800

    chore (comp): remove unnecessary import statement

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import BottomNavigation from 'material-ui/BottomNavigation';
-import BottomNavigationItem from 'material-ui/BottomNavigation';
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
 
 const Navigation = () => (
   <BottomNavigation>
